766929e37ff8b5dd0640ea3c8f566a7a9780bd77
emmavanvoorst <emmavanvoorst@gmail.com>
2023-07-03 21:21:23 -0400

    commit 2

--- client/src/components/ArtCalendar.js
+++ client/src/components/ArtCalendar.js
@@ -1,3 +1,15 @@
 import Calendar from 'react-calendar';
 import './App.css';
 
+const ArtCalendar = () => {
+    return (
+        <div className='app'>
+          <h1 className='text-center'>React Calendar</h1>
+          <div className='calendar-container'>
+            <Calendar />
+          </div>
+        </div>
+      );
+}
+
+export default ArtCalendar;
