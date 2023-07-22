d814646369ee177449e3698f281bb29b0c20425d
emmavanvoorst <emmavanvoorst@gmail.com>
2023-07-22 15:17:55 -0400

    added variable in server side index.js

--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ express()
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
+    res.header('Access-Control-Allow-Origin', '*');
     next();
   })
   .use(morgan("tiny"))
